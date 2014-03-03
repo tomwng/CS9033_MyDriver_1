0c0b3c2a4eb3489492203b98494f1ca72b3d3152
Tom <tomwng@gmail.com>
2014-03-03 15:17:28 -0500

    First commit

---
+++ MyDriver/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyDriver
+//
+//  Created by Tom on 3/2/14.
+//  Copyright (c) 2014 Tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DRVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DRVAppDelegate class]));
+	}
+}
